66d2a9b62305d7666d99371a4a85b17cdfcbec91
xieweibbz <xieweibbz@gmail.com>
2025-07-12 12:51:06 -0700

    Update transformer.py

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -35,13 +35,14 @@ class WeiEmbedding(nn.Module):
 class WeiRMSNorm(nn.Module):
 
   def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+    super(WeiRMSNorm, self).__init__()
     self.d_model = d_model
     self.eps = eps
-    super(WeiRMSNorm, self).__init__()
+    self.g = nn.Parameter(torch.ones(d_model, device=device, dtype=dtype))
 
   def forward(self, x: torch.Tensor) -> torch.Tensor:
     in_dtype = x.dtype
     x = x.to(torch.float32)
     avg = torch.sum(torch.square(x), dim=-1) / self.d_model
-    result = x / torch.unsqueeze(torch.sqrt(avg + self.eps), -1)
+    result = x / torch.unsqueeze(torch.sqrt(avg + self.eps), -1) * self.g
     return result.to(in_dtype)
