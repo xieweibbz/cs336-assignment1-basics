f4d6353ff448e8b11d1aa6d50601f0f6cfe9793d
xieweibbz <xieweibbz@gmail.com>
2025-07-12 12:18:16 -0700

    Update transformer.py

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -30,3 +30,18 @@ class WeiEmbedding(nn.Module):
     re = torch.index_select(self.embedding, dim=0, index=reshaped)
     result_shape = token_ids.shape + (self.embedding.shape[-1],)
     return torch.reshape(re, result_shape)
+
+
+class WeiRMSNorm(nn.Module):
+
+  def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+    self.d_model = d_model
+    self.eps = eps
+    super(WeiRMSNorm, self).__init__()
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    in_dtype = x.dtype
+    x = x.to(torch.float32)
+    avg = torch.sum(torch.square(x), dim=-1) / self.d_model
+    result = x / torch.unsqueeze(torch.sqrt(avg + self.eps), -1)
+    return result.to(in_dtype)
