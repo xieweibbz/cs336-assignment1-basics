9d4d1ee1665a25366f2057d1208b85999205dc83
xieweibbz <xieweibbz@gmail.com>
2025-07-12 12:52:02 -0700

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from torch import Tensor
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.transformer import WeiEmbedding
 from cs336_basics.transformer import WeiLinear
+from cs336_basics.transformer import WeiRMSNorm
 
 def run_linear(
     d_in: int,
@@ -386,7 +387,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    wei_rmsn = WeiRMSNorm(d_model, eps=eps)
+    wei_rmsn.g = nn.Parameter(weights)
+    return wei_rmsn(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
