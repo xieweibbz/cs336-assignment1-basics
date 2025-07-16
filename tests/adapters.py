bf15925d3f808e378f63c6cb7d3c9b264ee21c80
xieweibbz <xieweibbz@gmail.com>
2025-07-15 18:57:13 -0700

    Update adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -19,6 +19,7 @@ from cs336_basics.transformer import WeiRoPE
 from cs336_basics.transformer import wei_softmax
 from cs336_basics.transformer import WeiAttention
 from cs336_basics.transformer import WeiMultiHeadSelfAttention
+from cs336_basics.transformer import CopyMultiHeadSelfAttention
 
 def run_linear(
     d_in: int,
@@ -159,13 +160,16 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_out"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    self_att = WeiMultiHeadSelfAttention(d_model, num_heads, q_proj_weight.shape[-1], q_proj_weight.shape[-2], k_proj_weight.shape[-2], v_proj_weight.shape[-2])
-    self_att.w_q.w.data = q_proj_weight
-    self_att.w_k.w.data = k_proj_weight
-    self_att.w_v.w.data = v_proj_weight
-    self_att.w_o.w.data = o_proj_weight
-
-    return self_att(in_features)
+    # self_att = WeiMultiHeadSelfAttention(d_model, num_heads, q_proj_weight.shape[-1], q_proj_weight.shape[-2], k_proj_weight.shape[-2], v_proj_weight.shape[-2])
+    # self_att.w_q.w.data = q_proj_weight
+    # self_att.w_k.w.data = k_proj_weight
+    # self_att.w_v.w.data = v_proj_weight
+    # self_att.w_o.w.data = o_proj_weight
+    # return self_att(in_features)
+    mha = CopyMultiHeadSelfAttention(d_model, num_heads)
+    mha.w_qkv.W.data = torch.cat([q_proj_weight, k_proj_weight, v_proj_weight], dim=0)
+    mha.w_o.W.data = o_proj_weight
+    return mha(in_features)
 
     
 
