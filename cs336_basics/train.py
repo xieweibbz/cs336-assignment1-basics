2a8af7cde5aef4ef41e7ae7a1dda52e0907adc2c
xieweibbz <xieweibbz@gmail.com>
2025-07-20 16:17:43 -0700

    Update train.py

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 from einops import rearrange, einsum
 from jaxtyping import Float, Int
 from torch import Tensor
+from collections.abc import Callable, Iterable
+from typing import Optional
 
 
 def wei_log_softmax(in_features: Float[Tensor, " ..."], dim: int) -> Float[Tensor, " ..."]:
@@ -31,3 +33,48 @@ def wei_cross_entropy(inputs: Float[Tensor, " batch_size vocab_size"], targets: 
     log_softmax = wei_log_softmax(inputs, dim=-1)
     pro = log_softmax.gather(1, targets.unsqueeze(1)).squeeze(0)
     return torch.mean(-pro)
+
+
+class AdamWOptimizer(torch.optim.Optimizer):
+  def __init__(self, params, alpha=1e-3, beta1=0, beta2=0, theta=0, eps=0.00000001):
+    if alpha < 0:
+     raise ValueError(f"Invalid learning rate: {alpha}")
+    if beta1 < 0 or beta1 >= 1:
+     raise ValueError(f"Invalid learning parameter: {beta1}")
+    if beta2 < 0 or beta2 >= 1:
+     raise ValueError(f"Invalid learning parameter: {beta2}")
+    defaults = {"alpha": alpha, "beta1": beta1, "beta2": beta2, "theta": theta, "eps": eps}
+    super().__init__(params, defaults)
+
+    for group in self.param_groups:
+      for p in group["params"]:
+        state = self.state[p]
+        state["m"] = torch.zeros_like(p.data)
+        state["v"] = torch.zeros_like(p.data)
+        state["alpha_t"] = alpha
+
+  def step(self, closure: Optional[Callable] = None):
+    loss = None if closure is None else closure()
+    for group in self.param_groups:
+      alpha = group["alpha"] # Get the learning rate.
+      beta1 = group["beta1"]
+      beta2 = group["beta2"]
+      theta = group["theta"]
+      eps = group["eps"]
+      for p in group["params"]:
+        if p.grad is None:
+          continue
+        state = self.state[p] # Get state associated with p.
+        t = state.get("t", 0) # Get iteration number from the state, or initial value.
+        state["m"] = state["m"] * beta1 + (1 - beta1) * p.grad.data
+        state["v"] = state["v"] * beta2 + (1 - beta2) * p.grad.data**2
+        if t == 0:
+          state["alpha_t"] = alpha
+        else:
+          state["alpha_t"] = alpha * (1 - beta2**t)**0.5 / (1 - beta1**t)
+        
+        grad = p.grad.data # Get the gradient of loss with respect to p.
+        p.data -= state["alpha_t"] * state["m"] / (state["v"]**0.5 + eps) # Update weight tensor in-place.
+        p.data = p.data - alpha * theta * p.data# Update weight tensor in-place.
+        state["t"] = t + 1 # Increment iteration number.
+    return loss
