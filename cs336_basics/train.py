4e11e9fd6cd1092a8ab6c0be48d3694f87af9269
xieweibbz <xieweibbz@gmail.com>
2025-07-20 17:08:10 -0700

    Update train.py

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -77,3 +77,20 @@ class WeiAdamWOptimizer(torch.optim.Optimizer):
         state["t"] = t + 1 # Increment iteration number.
     return loss
 
+
+def copy_learning_rate_cosine_schedule(
+        it: int,
+        max_learning_rate: float,
+        min_learning_rate: float,
+        warmup_iters: int,
+        cosine_cycle_iters: int,
+) -> float:
+    if it < warmup_iters:
+        return max_learning_rate * it / warmup_iters
+    elif it < cosine_cycle_iters:
+        cos_percent = (it - warmup_iters) / (cosine_cycle_iters - warmup_iters)
+        return min_learning_rate + 0.5 * (max_learning_rate - min_learning_rate) * (
+            1 + math.cos(math.pi * cos_percent)
+        )
+    else:
+        return min_learning_rate
